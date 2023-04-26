643c41166265ee44d75222e4c1a0f05f5784d0a7
overload-ak <39258960+overload-ak@users.noreply.github.com>
2023-04-26 19:28:17 +0800

    test: pattern URL

---
+++ pattern.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"regexp"
+)
+
+const (
+	VariableStarPattern = `\{[a-zA-Z0-9_]+=*\*\}`
+	VariablePattern     = `\{[a-zA-Z0-9_]+\}`
+)
+
+type PathPattern struct {
+	pattern string
+	re      *regexp.Regexp
+}
+
+func NewPathPattern(pattern string) *PathPattern {
+	regexPattern := convertToRegexPattern(pattern)
+	return &PathPattern{
+		pattern: pattern,
+		re:      regexp.MustCompile(regexPattern),
+	}
+}
+
+func (p *PathPattern) Match(url string) bool {
+	return p.re.MatchString(url)
+}
+
+func convertToRegexPattern(pattern string) string {
+	re := regexp.MustCompile(VariableStarPattern)
+	regexPattern := re.ReplaceAllString(pattern, "(.*)")
+	re = regexp.MustCompile(VariablePattern)
+	regexPattern = re.ReplaceAllString(regexPattern, "([^/]+)")
+	regexPattern = "^" + regexPattern + `(\?.*)?$`
+	return regexPattern
+}
