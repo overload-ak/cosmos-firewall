4cf80e3f1c7fb86644d87c20c31ab275d66c21a2
overload-ak <39258960+overload-ak@users.noreply.github.com>
2023-04-28 16:41:28 +0800

    feat: add handler

---
+++ internal/handler/jsonrpc.go
@@ -0,0 +1,106 @@
+package handler
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/pkg/errors"
+	"github.com/tendermint/tendermint/rpc/jsonrpc/server"
+	"github.com/tendermint/tendermint/rpc/jsonrpc/types"
+
+	"github.com/overload-ak/fx-firewall/internal/middleware"
+	"github.com/overload-ak/fx-firewall/logger"
+)
+
+func JSONRPCHandler(validator middleware.Validator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			jsonRpcResponse(w, http.StatusInternalServerError, types.RPCInvalidParamsError(nil, err))
+			return
+		}
+		logger.Infof("JSONRPC Method: [%s], RequestURI: [%s], Port: [%s]", r.Method, r.URL.RequestURI())
+		logger.Info("JSONRPC request body base64: ", base64.StdEncoding.EncodeToString(body))
+		path := r.URL.Path
+		if !validator.IsJSONPRCPathAllowed(path) {
+			jsonRpcResponse(w, http.StatusMethodNotAllowed, types.RPCMethodNotFoundError(nil))
+			return
+		}
+		if len(path) < 1 {
+			var requests []types.RPCRequest
+			if err = json.Unmarshal(body, &requests); err != nil {
+				var request types.RPCRequest
+				if err = json.Unmarshal(body, &request); err != nil {
+					jsonRpcResponse(w, http.StatusInternalServerError, types.RPCParseError(err))
+					return
+				}
+				requests = []types.RPCRequest{request}
+			}
+			for _, rpcRequest := range requests {
+				request := rpcRequest
+				if request.ID == nil {
+					logger.Debug(
+						"HTTPJSONRPC received a notification, skipping... (please send a non-empty ID if you want to call a method)",
+						"req", request,
+					)
+					continue
+				}
+				if len(request.Params) > 0 {
+					if request.Method == "broadcast_tx_commit" || request.Method == "check_tx" ||
+						request.Method == "broadcast_tx_sync" || request.Method == "broadcast_tx_async" {
+						txBytes, err := getTxBytesFromParams(request.Params)
+						if err != nil {
+							jsonRpcResponse(w, http.StatusInternalServerError, types.RPCInvalidParamsError(request.ID, err))
+							return
+						}
+						if err = validator.CheckTxBytes(txBytes); err != nil {
+							jsonRpcResponse(w, http.StatusInternalServerError, types.RPCInternalError(nil, err))
+							return
+						}
+					}
+				}
+			}
+		}
+		// todo success response
+		jsonRpcResponse(w, http.StatusOK, types.NewRPCSuccessResponse(nil, nil))
+	}
+}
+
+func getTxBytesFromParams(data json.RawMessage) ([]byte, error) {
+	var raw map[string]json.RawMessage
+	if err := json.Unmarshal(data, &raw); err == nil {
+		rawTx, ok := raw["tx"]
+		if !ok {
+			return nil, fmt.Errorf("undefined transaction type. params: %v. Expected map ", rawTx)
+		}
+		var txBytes []byte
+		if err := json.Unmarshal(rawTx, &txBytes); err != nil {
+			return nil, fmt.Errorf("json unmarshal txBytes error: %s", err.Error())
+		}
+		return txBytes, nil
+	} else {
+		logger.Warnf("json unmarshal raw error: %s", err.Error())
+	}
+	var raws []json.RawMessage
+	if err := json.Unmarshal(data, &raws); err == nil {
+		var txBytes []byte
+		if len(raws) > 0 {
+			if err := json.Unmarshal(raws[0], &txBytes); err != nil {
+				return nil, fmt.Errorf("json unmarshal raws txBytes error: %s", err.Error())
+			}
+		}
+		return txBytes, nil
+	} else {
+		logger.Warnf("json unmarshal raws error: %s", err.Error())
+	}
+	return nil, errors.New("unknown type tx raw message")
+}
+
+func jsonRpcResponse(writer http.ResponseWriter, code int, res types.RPCResponse) {
+	if err := server.WriteRPCResponseHTTPError(writer, code, res); err != nil {
+		logger.Error("failed to write response", "res", res, "err", err)
+	}
+}
---
+++ internal/handler/rest.go
@@ -0,0 +1,133 @@
+package handler
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/gogo/protobuf/proto"
+
+	"github.com/overload-ak/fx-firewall/internal/middleware"
+	"github.com/overload-ak/fx-firewall/logger"
+)
+
+func RestHandler(validator middleware.Validator) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		body, err := io.ReadAll(request.Body)
+		if err != nil {
+			restResponse(writer, http.StatusInternalServerError, "read all body error: ", nil)
+			return
+		}
+		logger.Infof("REST Method: [%s], RequestURI: [%s], Port: [%s]", request.Method, request.URL.RequestURI())
+		logger.Info("REST request body base64: ", base64.StdEncoding.EncodeToString(body))
+		url := request.URL.RequestURI()
+
+		if !validator.IsRESTPathAllowed(url) {
+			restResponse(writer, http.StatusMethodNotAllowed, "method not allowed", nil)
+			return
+		}
+		switch url {
+		case "/cosmos/tx/v1beta1/simulate":
+			simulateReq := tx.SimulateRequest{}
+			if err = proto.Unmarshal(body, &simulateReq); err != nil {
+				if err = json.Unmarshal(body, &simulateReq); err != nil {
+					type BroadcastTxRequest struct {
+						TxBytes []byte `json:"tx_bytes,omitempty"`
+						Mode    string `json:"mode,omitempty"`
+					}
+					var req1 BroadcastTxRequest
+					if err = json.Unmarshal(body, &req1); err != nil {
+						restResponse(writer, http.StatusInternalServerError, fmt.Sprintf("broadcastTxRequest json unmarshal %v", err.Error()), nil)
+						return
+					}
+					simulateReq.TxBytes = req1.TxBytes
+				}
+			}
+			if err != nil {
+				restResponse(writer, http.StatusInternalServerError, fmt.Sprintf("simulateRequest json unmarshal %v", err.Error()), nil)
+				return
+			}
+			if simulateReq.Tx != nil {
+				for _, signature := range simulateReq.Tx.Signatures {
+					if len(signature) != 64 && len(signature) != 65 {
+						restResponse(writer, http.StatusNonAuthoritativeInfo, "signature format error", nil)
+						return
+					}
+				}
+				if err = validator.CheckTxAuthInfo(*simulateReq.Tx.AuthInfo); err != nil {
+					restResponse(writer, http.StatusUnprocessableEntity, err.Error(), nil)
+					return
+				}
+
+				if err = validator.CheckTxBody(*simulateReq.Tx.Body); err != nil {
+					restResponse(writer, http.StatusUnprocessableEntity, err.Error(), nil)
+					return
+				}
+			}
+			if simulateReq.TxBytes != nil {
+				if err = validator.CheckTxBytes(simulateReq.TxBytes); err != nil {
+					restResponse(writer, http.StatusUnprocessableEntity, err.Error(), nil)
+					return
+				}
+			}
+		case "/cosmos/tx/v1beta1/txs":
+			var req tx.BroadcastTxRequest
+			if err = json.Unmarshal(body, &req); err != nil {
+				type BroadcastTxRequest struct {
+					TxBytes []byte `json:"tx_bytes,omitempty"`
+					Mode    string `json:"mode,omitempty"`
+				}
+				var req1 BroadcastTxRequest
+				if err = json.Unmarshal(body, &req1); err != nil {
+					restResponse(writer, http.StatusInternalServerError, fmt.Sprintf("json unmarshal BroadcastTxRequest error: %s", err.Error()), nil)
+					return
+				}
+				req.TxBytes = req1.TxBytes
+				req.Mode = tx.BroadcastMode(tx.BroadcastMode_value[req1.Mode])
+			}
+			if req.TxBytes == nil {
+				restResponse(writer, http.StatusInternalServerError, "invalid empty tx bytes", nil)
+				return
+			}
+			switch req.Mode {
+			case tx.BroadcastMode_BROADCAST_MODE_UNSPECIFIED:
+			case tx.BroadcastMode_BROADCAST_MODE_BLOCK:
+			case tx.BroadcastMode_BROADCAST_MODE_SYNC:
+			case tx.BroadcastMode_BROADCAST_MODE_ASYNC:
+			}
+			if err = validator.CheckTxBytes(req.TxBytes); err != nil {
+				restResponse(writer, http.StatusUnprocessableEntity, err.Error(), nil)
+				return
+			}
+		}
+		// todo success response
+	}
+}
+
+type Response struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func restResponse(writer http.ResponseWriter, code int, msg string, data interface{}) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(code)
+	response := &Response{
+		Code: code,
+		Msg:  msg,
+		Data: data,
+	}
+	d, err := json.Marshal(response)
+	logger.Infof("http response statusCode: %d, body: %s", code, base64.StdEncoding.EncodeToString(d))
+	if err != nil {
+		logger.Errorf("output json marshal error: %s", err.Error())
+	} else {
+		if _, err := writer.Write(d); err != nil {
+			logger.Errorf("output write error: %s", err.Error())
+		}
+	}
+}
