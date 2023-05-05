06b86bb6157bbfa92555813697ddf72ab82ef3a4
overload-ak <39258960+overload-ak@users.noreply.github.com>
2023-05-05 18:21:08 +0800

    feat: add list and verify cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,95 +1,118 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 
 	"github.com/overload-ak/cosmos-firewall/config"
-	"github.com/overload-ak/cosmos-firewall/internal/handler"
 	"github.com/overload-ak/cosmos-firewall/internal/middleware"
 	"github.com/overload-ak/cosmos-firewall/logger"
 )
 
 const (
-	networkFlag  = "network"
-	logLevelFlag = "log-level"
-	configFlag   = "config"
+	flagLogLevel                    = "log_level"
+	flagRpcAddress                  = "rpc_address"
+	flagGrpcAddress                 = "grpc_address"
+	flagRestAddress                 = "rest_address"
+	flagChainId                     = "chain.chain_id"
+	flagForward                     = "chain.forward"
+	flagJsonRpc                     = "chain.json_rpc"
+	flagGrpc                        = "chain.grpc"
+	flagRest                        = "chain.rest"
+	flagMinimumGasLimit             = "chain.minimum_gas_limit"
+	flagMinimumFee                  = "chain.minimum_fee"
+	flagMaxMemo                     = "chain.max_memo"
+	flagWhiteRouters                = "chain.white_routers"
+	flagExtensionOptions            = "chain.extension_options"
+	flagNonCriticalExtensionOptions = "chain.non_critical_extension_options"
+	flagGranter                     = "chain.granter"
+	flagPayer                       = "chain.payer"
+	flagSignerInfos                 = "chain.signer_infos"
+	flagMinimumSignatures           = "chain.minimum_signatures"
+	flagPublicKeyTypeUrl            = "chain.public_key_type_url"
+
+	flagRequestType = "request_type"
 )
 
 func main() {
 	rootCmd := &cobra.Command{
 		Use:   "firewall",
-		Short: "FunctionX Chain firewall",
+		Short: "Cosmos firewall",
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			if err := viper.BindPFlags(cmd.Flags()); err != nil {
 				return err
 			}
 			// Initialize log level
-			logger.Init(viper.GetString(logLevelFlag))
+			logger.Init(viper.GetString(flagLogLevel))
+			// Set the configuration file name and path
+			viper.SetConfigName("config")
+			viper.SetConfigType("toml")
+			viper.AddConfigPath("./config")
 			return nil
 		},
 	}
 	rootCmd.AddCommand(start())
-
-	rootCmd.PersistentFlags().String(networkFlag, "local", "set network")
-	rootCmd.PersistentFlags().String(logLevelFlag, "info", "the logging level (debug|info|warn|error|dpanic|panic|fatal)")
+	rootCmd.AddCommand(verify())
+	rootCmd.AddCommand(list())
+	rootCmd.PersistentFlags().String(flagLogLevel, "info", "the logging level (debug|info|warn|error|dpanic|panic|fatal)")
+	rootCmd.PersistentFlags().StringP(flagChainId, "c", "", "the chain id")
 	SilenceCmdErrors(rootCmd)
 	CheckErr(rootCmd.Execute())
 }
 
-func start() *cobra.Command {
-	rootCmd := &cobra.Command{
-		Use:   "start",
-		Short: "Start firewall",
+func verify() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "verify [router]",
+		Short: "verify router is allowed in",
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			cfg, err := config.ReadConfig(viper.GetString(configFlag))
-			if err != nil {
-				return err
+			chainId := viper.GetString(flagChainId)
+			requestType := viper.GetString(flagRequestType)
+			validator := middleware.NewValidator(&config.Config{Chain: config.ChainConfig{ChainID: chainId}})
+			isVerify := false
+			switch requestType {
+			case "grpc":
+				isVerify = validator.IsGRPCRouterAllowed(args[0])
+			case "rest":
+				isVerify = validator.IsRESTRouterAllowed(args[0])
+			default:
+				isVerify = validator.IsJSONPRCRouterAllowed(args[0])
 			}
-			return Run(cfg)
+			if !isVerify {
+				logger.Warnf("Router: \"%s\" is not allowed", args[0])
+				return nil
+			}
+			logger.Infof("Router: \"%s\" is allowed", args[0])
+			return nil
 		},
 	}
-	rootCmd.Flags().String(configFlag, "/Users/lee/wokoWorks/go_code/deso/cosmos-firewall/config/config.json", "")
-	return rootCmd
+	cmd.Flags().String(flagRequestType, "jsonrpc", "request type (jsonrpc|grpc|rest)")
+	return cmd
 }
 
-func Run(config *config.Config) error {
-	validator := middleware.NewValidator(config)
-	go RunJSONRPCServer(validator)
-	go RunRESTServer(validator)
-	return RunGRPCServer(validator)
-}
-
-func RunGRPCServer(validator middleware.Validator) error {
-	logger.Infof("start GRPC server listening on %v", ":9090")
-	h2s := &http2.Server{}
-	srv := &http.Server{
-		Addr:    ":9090",
-		Handler: h2c.NewHandler(handler.GRPCHandler(validator), h2s),
+func list() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list [chainId] [request_type] ",
+		Short: "verify router is allowed in",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			validator := middleware.NewValidator(&config.Config{Chain: config.ChainConfig{ChainID: args[0]}})
+			var routers []string
+			switch args[1] {
+			case "grpc":
+				routers = validator.Routers.GetGRPCRouters()
+			case "rest":
+				routers = validator.Routers.GetRESTRouters()
+			default:
+				routers = validator.Routers.GetRPCRouters()
+			}
+			logger.Infof("====== %v total Routers: %v ======", args[1], len(routers))
+			for _, router := range routers {
+				logger.Info(router)
+			}
+			logger.Infof("====== end ======")
+			return nil
+		},
 	}
-	if err := http2.ConfigureServer(srv, &http2.Server{}); err != nil {
-		return err
-	}
-	if err := srv.ListenAndServe(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func RunRESTServer(validator middleware.Validator) {
-	logger.Infof("start REST server listening on %v", ":1317")
-	if err := http.ListenAndServe(":1317", handler.RestHandler(validator)); err != nil {
-		panic(err)
-	}
-}
-
-func RunJSONRPCServer(validator middleware.Validator) {
-	logger.Infof("start JSON-RPC server listening on %v", ":26657")
-	if err := http.ListenAndServe(":26657", handler.JSONRPCHandler(validator)); err != nil {
-		panic(err)
-	}
+	return cmd
 }
